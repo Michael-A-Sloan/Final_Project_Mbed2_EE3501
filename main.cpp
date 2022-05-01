2fdc425f4c9bd9b1e30dd5bbfcd3dcf12a7228ec
Michael Sloan <michaelallansloan@gmail.com>
2022-05-01 04:04:40 +0000

    Setup The format for Time and AM PM as well as fixed the new keypad.

--- main.cpp
+++ main.cpp
@@ -12,25 +12,26 @@ TextLCD lcd(PA_0, PA_1, PA_4, PB_0, PC_1, PC_0); // rs, e, d4-d7
 InterruptIn star(PA_8);
 InterruptIn Button(USER_BUTTON);
 
-DigitalOut ROW1 (PA_7);
-DigitalOut ROW2 (PB_6);
-DigitalOut ROW3 (PC_7);
-DigitalOut ROW4 (PA_9);
+DigitalOut ROW1 (PA_8); 
+DigitalOut ROW2 (PB_10);
+DigitalOut ROW3 (PB_4); 
+DigitalOut ROW4 (PB_5); 
 
-DigitalIn COL1 (PA_8,PullUp);
-DigitalIn COL2 (PB_10,PullUp);
-DigitalIn COL3 (PB_4,PullUp);
-DigitalIn COL4 (PB_5,PullUp);
+DigitalIn COL1 (PA_7,PullUp);
+DigitalIn COL2 (PB_6,PullUp);
+DigitalIn COL3 (PC_7,PullUp);
+DigitalIn COL4 (PA_9,PullUp);
 
-AnalogIn LM35(PA_6); // board pin
+AnalogIn LM35(PC_3); // board pin
 
-int Sec = 30; // time in seconds
+int Sec = 50; // time in seconds
 int Min = 59; // time in minutes
 int Hr = 12;// time in hours
 //float Temp = TempIn; // temperature C or F
 int Volt; // voltage that controls temperature sensor
 char Unit = 'C'; // C stands for celcius
-string AMPM; // time in 12 hour format
+int AMPM = 2; // time in 12 hour format
+string Time = "AM"; //For switch AM PM 
 float tempC,tempF,a[10],avg;
 
 
@@ -127,21 +128,38 @@ void normal() //Start of Normal Clock Mode
    
    Sec = Sec + 1;
 
-   if (Sec == 60)   /* seconds is equal to 60 then again start from zero and add an increment of one in the minute value */
+    if (Sec == 60)   /* seconds is equal to 60 then again start from zero and add an increment of one in the minute value */
     {
-        Sec = 0;
+        Sec = 00;
         Min = Min + 1;
     }
     if (Min == 60) /* if minute is equal to 60 then again start from zero and add an increment of one in the hour value */
     {
-        Min = 0;
+        Min = 00;
         Hr = Hr + 1;
-        //AMPM = AMPM + 1;
     }
     if (Hr == 13) /* if hour value is 13 then replace its value from 13 to 1 to change it to 12 hour format*/
     {
-        Hr = 1;
+        Hr = 01;
+
+        switch (AMPM)
+        {
+            case 1:
+                Time = "AM";
+                AMPM ++;
+                break;
+            case 2:
+                Time = "PM";
+                AMPM --;
+                break;
+        }
+    
     }
+
+ /*   else if (Hr ==13 && AMPM == "PM")
+    {
+        AMPM = "AM";
+    }*/
 
 } //End of Normal Clock Mode
 
@@ -204,21 +222,24 @@ int main()
 
     while (1) 
     {
-        cout << "Second" << Sec << "Min" << Min << endl;
-        cout << keypad_scan() << endl;
+        cout << "Hr " << Hr << " Min " << Min << " Sec " << Sec << " " << Time << " "  << AMPM << endl;
+        cout << "Keypad Press: " << keypad_scan() << endl;
 
         wait(1); // wait 1 miliseconds
         normal(); // normal displays the input of enter minute, hour, second
    //     Temperature(); // temperature method to print farenheight or celcius
         lcd.cls (); // lcd cls clears the display
 
-        lcd.printf ("%i",Hr); // print hours
+        lcd.printf ("Time ");
+        lcd.printf ("%02i",Hr); // print hours used %02i for an integer with 2 digits and Preceeding Zeros.
         lcd.printf ( ":" );
-        lcd.printf ("%i",Min); // print minutes
+        lcd.printf ("%02i",Min); // print minutes used %02i for an integer with 2 digits and Preceeding Zeros.
         lcd.printf ( ":" );
-        lcd.printf ("%i",Sec); // print seconds
+        lcd.printf ("%02i",Sec); // print seconds used %02i for an integer with 2 digits and Preceeding Zeros.
+        lcd.printf (" ");
+        lcd.printf ("%s", Time);
         lcd.locate (0,2);
-        lcd.printf (" Temp ");
+        lcd.printf ("Temp ");
         lcd.printf ("%i",LM35); // print temperature in F or C
 
 //test
